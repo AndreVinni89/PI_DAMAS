fa99360ddbf3386430698bc4d1bcd393c4adcf08
AndreVinni89 <andrevinnicius89@gmail.com>
2020-09-09 08:56:39 -0300

    a5t

---
+++ PI_DAMAS/src/classes/Jogador.java
@@ -0,0 +1,30 @@
+package classes;
+
+public class Jogador {
+	public int pontuacao;
+	public String nickname;
+	public int numVitorias;
+	public int numDerrotas;
+	public int numEmpates;
+	
+	public Jogador(String nick) {
+
+		nickname = nick;
+		
+		
+	}
+
+
+	
+	public void jogar() {
+		
+		System.out.println("Estou jogando");
+		
+	}
+	
+	
+	
+	
+	
+	
+}
